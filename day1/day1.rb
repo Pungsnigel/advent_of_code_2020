abb0d29175588dfdc60f9ce71ec7792ddce9f419
Jesper Lugner <jespert.persson@gmail.com>
2020-12-01 22:26:21 +0100

    remove benchmarks

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,28 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'benchmark'
-
 input = File.readlines('./input.txt')
 
-Benchmark.bm do |benchmark|
-  benchmark.report do
-    result = input
-             .map(&:to_i)
-             .combination(2)
-             .select { |a, b| a + b == 2020 }
-             .flatten
-             .reduce(:*)
-    puts "Day1 first part: #{result}"
-  end
+result = input.map(&:to_i).combination(2).select { |a, b| a + b == 2020 }.flatten.reduce(:*)
+puts "Day1 first part: #{result}"
 
-  benchmark.report do
-    result = input
-             .map(&:to_i)
-             .combination(3)
-             .select { |a, b, c| a + b + c == 2020 }
-             .flatten
-             .reduce(:*)
-    puts "Day1 second part: #{result}"
-  end
-end
+result = input.map(&:to_i).combination(3).select { |a, b, c| a + b + c == 2020 }.flatten.reduce(:*)
+puts "Day1 second part: #{result}"
