5e2b322084a7a58f23a9a4a344e4f7ea38b57fed
Jesper Lugner <jespert.persson@gmail.com>
2020-12-01 22:38:41 +0100

    Replace select with find and remove redundant flatten

--- day1/day1.rb
+++ day1/day1.rb
@@ -3,8 +3,8 @@
 
 input = File.readlines('./input.txt')
 
-result = input.map(&:to_i).combination(2).select { |a, b| a + b == 2020 }.flatten.reduce(:*)
+result = input.map(&:to_i).combination(2).find { |a, b| a + b == 2020 }.reduce(:*)
 puts "Day1 first part: #{result}"
 
-result = input.map(&:to_i).combination(3).select { |a, b, c| a + b + c == 2020 }.flatten.reduce(:*)
+result = input.map(&:to_i).combination(3).find { |a, b, c| a + b + c == 2020 }.reduce(:*)
 puts "Day1 second part: #{result}"
