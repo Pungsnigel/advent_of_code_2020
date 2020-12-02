81c79656e93610bed15dd02e8936423f9ecd7f51
Jesper Lugner <jespert.persson@gmail.com>
2020-12-02 07:44:48 +0100

    Add day 2

---
+++ day2/day2.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+input = File.readlines('./input.txt')
+
+valid_passwords = input.count do |attempt|
+  range, char, password = attempt.split(' ')
+  low, high = range.split('-').map(&:to_i)
+  (low..high).cover?(password.count(char))
+end
+
+puts "Solution for day 2 part a: #{valid_passwords}"
+
+valid_passwords = input.count do |attempt|
+  positions, char, password = attempt.delete(':', '').split(' ')
+  a, b = positions.split('-').map(&:to_i).map(&:pred)
+  (password[a] == char || password[b] == char) && (password[a] != password[b])
+end
+
+puts "Solution for day 2 part b: #{valid_passwords}"
